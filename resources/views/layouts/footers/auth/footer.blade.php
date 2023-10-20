d9f50be3022aad17d33324813f66a92162d52c02
Michael Erameh <eramehmichael2000@gmail.com>
2023-10-20 05:49:06 +0100

    changes

---
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="footer pt-3  ">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+                <div class="copyright text-center text-sm text-muted text-lg-start">
+                    © <script>
+                        document.write(new Date().getFullYear())
+                    </script>
+                   <a style="color: #252f40;" href="" class="font-weight-bold ml-1" target="_blank">Mykaeltech</a>
+
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                    <li class="nav-item">
+                        <a href="https://www.mykaletech.com" class="nav-link text-muted" target="_blank">Mykaeltech</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-muted" target="_blank">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-muted" target="_blank">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
